09c543a5e220691fabb922b55e4f0628d990db09
Red-p <proietti99@gmail.com>
2022-07-16 12:08:20 +0200

    Speriamo funzioni

--- src/App.js
+++ src/App.js
@@ -29,14 +29,14 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path="/test-directory" element={<h1>Home Page</h1>} />
+          <Route exact path="/" element={<h1>Home Page</h1>} />
           <Route exact path="page1" element={<Page1 />} />
           <Route exact path="page2" element={<Page2 />} />
           <Route exact path="page3" element={<Page3 />} />
         </Routes>
         <div className="list">
           <ul>
-            <li><Link to="/test-directory">Home</Link></li>
+            <li><Link to="/">Home</Link></li>
             <li><Link to="page1">Page 1</Link></li>
             <li><Link to="page2">Page 2</Link></li>
             <li><Link to="page3">Page 3</Link></li>
